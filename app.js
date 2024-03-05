6a2ee27fa904e3c0b8afac5a2504dfa6ee836a80
racheli1203 <rachelim1203@gmail.com>
2024-03-05 14:58:27 +0200

    add

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require("express")
-// const mongoose = require("mongoose");//conect to mongo
-const app = express()
-const port = 8081
-const bodyParser =require('body-parser')
-const userRouter=require('./router/routing')
 
 const express = require("express");
 const mongoose = require("mongoose"); 
 
-// // Connect to MongoDB
-// mongoose.connect("mongodb://127.0.0.1:27017/mydatabase", {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+const app = express();
+const port = 8081;
+const bodyParser = require("body-parser");
+const userRouter = require("./router/routing");
 
-// mongoose.connection.on("error", console.error.bind(console, "MongoDB connection error:"));
-// mongoose.connection.once("open", () => {
-//   console.log("Connected to MongoDB database");
-// });
+app.use(bodyParser.json());
+app.use("/users", userRouter);
 
+mongoose.connect("mongodb://127.0.0.1:27017/mydatabase", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.listen(port, () => {
-    console.log("app is runing!!")
-})
+mongoose.connection.on("error", console.error.bind(console, "MongoDB connection error:"));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB database");
+  app.listen(port, () => {
+    console.log("App is running!!");
+  });
+  
+});
--- module/userModule.js
+++ module/userModule.js
@@ -8,38 +8,19 @@ const userSchema = new mongoose.Schema({
         unique:true
     } 
 },
+
 {timestamps:true}
 );
 
 const UserModel = mongoose.model('User', userSchema);
 
 class User {
-    
-    constructor(id, name, phone, email) {
-        this.id = id;
-        this.name = name;
-        this.phone = phone;
-        this.email = email;
-    }
-    static users = [{ id: 1, name: "racheli", phone: "852", email: "741@gmail.com" }, { id: 2, name: "yael", phone: "852", email: "741@gmail.com" }];
-    
-
-    static getUserById(userId) {
-        const user = this.users.find(user => user.id === userId);
-        return user;
-    }
-    
-    static createUser(userData) {
-        const { id, name, phone, email } = userData;
-        const newUser = new User(id, name, phone, email);
-        this.users.push(newUser);
-        return newUser;
-    }
-    
-    static putUser(userId, putUserData) {
-        const userToUpdate = this.getUserById(userId);
-        if (!userToUpdate) {
-            throw new Error('User not found');
+    static async getUserById(userId) {
+        try {
+            const user = await UserModel.findById(userId);
+            return user;
+        } catch (error) {
+            throw new Error('Error fetching user by ID');
         }
     }
     
