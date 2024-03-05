adf7033824fd08a580c122c79a36877c3ec0fdbe
racheli1203 <rachelim1203@gmail.com>
2024-03-05 10:51:06 +0200

    end validator

--- userValidator.js
+++ userValidator.js
@@ -3,7 +3,7 @@ class UserValidator {
     static validateUserData(req, res, next) {
         const { name, phone, email } = req.body;
 
-   
+
         if (!name || !phone || !email) {
             return res.status(400).send('All fields are required');
         }
@@ -11,19 +11,21 @@ class UserValidator {
         if (!UserValidator.isValidEmail(email)) {
             return res.status(400).send('Invalid email format');
         }
-      
+
         if (name.length < 2) {
-           res.send('Name must be at least two characters long').status(400);
+            res.send('Name must be at least two characters long').status(400);
         }
 
         if (phone.length !== 10 || isNaN(parseInt(phone))) {
-           res.send('Phone number must be 10 digits').status(400);
-           next();
-    } 
+            res.send('Phone number must be 10 digits').status(400);
+
+        }
+        next();
+    }
     static isValidEmail(email) {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return emailRegex.test(email);
     }
-  }
 }
+
 module.exports = UserValidator;
